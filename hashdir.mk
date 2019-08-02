050128e3aaf82215d966baa440757946f69e3c78
Takashi SASAKI <takashi316@gmail.com>
2019-08-02 22:33:53 +0900

    never use PRIMARYKEY

--- hashdir.mk
+++ hashdir.mk
@@ -1,8 +1,8 @@
 .ONESHELL:
 SHELL=/bin/bash
 
-ifndef PRIMARYKEY_SUFFIX
-  PRIMARYKEY_SUFFIX:=primarykey
+ifndef HASHDIR_SOURCE_WILDCARD
+  HASHDIR_SOURCE_WILDCARD=*.txt
 endif
 
 ifndef HASH_STRING_LENGTH
@@ -23,26 +23,28 @@ HASH_COMMAND=$(MD5_HASH_COMMAND)
 #HASH_COMMAND=$(GIT_HASH_COMMAND)
 
 
-PRIMARYKEY_FILES:=$(wildcard ./*.$(PRIMARYKEY_SUFFIX))
+$(info HASHDIR_SOURCE_WILDCARD=$(HASHDIR_SOURCE_WILDCARD))
+HASHDIR_SOURCE_FILES:=$(wildcard $(HASHDIR_SOURCE_WILDCARD))
+$(info HASHDIR_SOURCE_FILES=$(HASHDIR_SOURCE_FILES))
 
-PRIMARYKEY_FILES_ALLINONE:=$(shell mktemp -u)
-PRIMARYKEY_FILES_ALLINONE_HASH:=$(shell mktemp -u)
-PRIMARYKEY_FILES_ALLINONE_HASH_SHORT:=$(shell mktemp -u)
+HASHDIR_SOURCE_ALLINONE:=$(shell mktemp -u)
+HASHDIR_SOURCE_ALLINONE_HASH:=$(shell mktemp -u)
+HASHDIR_SOURCE_ALLINONE_HASH_SHORT:=$(shell mktemp -u)
 
-$(PRIMARYKEY_FILES_ALLINONE): $(PRIMARYKEY_FILES)
+$(HASHDIR_SOURCE_ALLINONE): $(HASHDIR_SOURCE_FILES)
 	cat $^ | sort -u | sed -r -e '/^$$/d' | tee $@
 
-$(PRIMARYKEY_FILES_ALLINONE_HASH): $(PRIMARYKEY_FILES_ALLINONE)
-	cat $< | (while read line; do (echo -n "$$line" | $(MD5_HASH_COMMAND); done) | tee $@
+$(HASHDIR_SOURCE_ALLINONE_HASH): $(HASHDIR_SOURCE_ALLINONE)
+	cat $< | (while read line; do (echo -n "$$line" | $(HASH_COMMAND); done) | tee $@
 
-$(PRIMARYKEY_FILES_ALLINONE_HASH_SHORT): $(PRIMARYKEY_FILES_ALLINONE_HASH)
+$(HASHDIR_SOURCE_ALLINONE_HASH_SHORT): $(HASHDIR_SOURCE_ALLINONE_HASH)
 	cat $< | sed -n -r -e 's/^([0-9a-f]{$(HASH_STRING_LENGTH)}).*/\1/p' | tee $@
 
 debug-hashdir: make-hashdir
 
-make-hashdir: $(PRIMARYKEY_FILES_ALLINONE_HASH_SHORT) 
-	exec 3<$(PRIMARYKEY_FILES_ALLINONE_HASH_SHORT)
-	exec 4<$(PRIMARYKEY_FILES_ALLINONE)
+make-hashdir: $(HASHDIR_SOURCE_ALLINONE_HASH_SHORT) 
+	exec 3<$(HASHDIR_SOURCE_ALLINONE_HASH_SHORT)
+	exec 4<$(HASHDIR_SOURCE_ALLINONE)
 	(while read -u 3 md5; do (mkdir -p $${md5}; read -u 4 path; echo -n $${path} > $${md5}/dir.txt); done)
 	exec 3<&-
 	exec 4<&-
